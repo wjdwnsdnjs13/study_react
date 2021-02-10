b6651b1a442f4fe6f7cbbec7d2904d6a4fad92d7
wjdwnsdnjs13 <wjdwnsdnjs13@naver.com>
2021-02-10 09:31:18 +0000

    0210

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function Food({fav}){
     console.log(fav)
     return <h1>I like {fav}</h1>;
 }
+function study({day, gita, explain}){
+    return <h1>{day} {gita} {explain}</h1>;
+}
 
 function App() {
 
@@ -15,6 +18,7 @@ function App() {
           <Food fav="ramen"/>
           <Food fav="samgiopsal"/>
           <Food fav="chukumi"/>
+          <study day="0210" gita="https://ko.javascript.info/import-export#ref-4122" explain="as와 default, import와 export에 대한 설명이 된 사이트" />
     </div>
   );
 }
